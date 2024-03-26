939424d4a54c9269be64a015ec632bfd5bf6e71d
myazraa <vitalysedelnikov5@gmail.com>
2024-03-26 16:08:49 +0300

    Test
    Test

---
+++ lab1 Gaysse/Lab1/Lab1/Lab1.cpp
@@ -0,0 +1,111 @@
+﻿// Метод Гаусса
+#include <iostream>
+#include <string>
+#include <locale>
+
+using namespace std;
+double x[6];
+
+void PrintMatrix(double(&a)[5][6]) {// Вывод матрицы
+	int i, j = 0;
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 6; j++) {
+			printf_s("  % 5.3f", a[i][j]);
+		}
+		printf_s("\n");
+	}
+}
+
+
+
+void Zamena(double(&a)[5][6], int col, int row) {// Смена местами строк 
+	double arr[6];
+	for (int j = 0; j < 6; j++) {
+		arr[j] = a[col][j];
+		a[col][j] = a[row][j];
+		a[row][j] = arr[j];
+	}
+}
+
+void Nulls(double(&a)[5][6], int p) {// Проверка нулей на главной диагонали	
+	if (a[p][p] == 0) // Если на главной диагонали найден ноль
+	{
+		int m = p + 1;
+		while (a[p][p] == 0) {
+			if ((a[m][p] != 0)) //|| (m >= 5))
+			{
+				Zamena(a, m, p);
+				cout << endl;
+				PrintMatrix(a);
+			}
+			m++;
+		}
+	}
+}
+
+void MultDiag(double(&a)[5][6], int p) {
+	for (int i = p + 1; i < 5; i++)//Произведение элементов главной диагонали минус произведение элементов на побочной диагонали
+	{
+		for (int j = p + 1; j < 6; j++)
+		{
+			if (i != p && j > p)
+			{
+				a[i][j] = (a[i][j] * a[p][p] - a[i][p] * a[p][j]) / a[p][p];
+
+			}
+		}
+	}
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	double matrix[5][6]{
+			{-0.38,0.88,-0.4,-0.16,0.56,-0.79},
+			{0.75,0.88,0.46,0.32,0.16,0.07},
+			{-0.68,0.64,0.57,-0.88,-0.4,-0.52},
+			{0.48,-0.27,-0.45,-0.01,-0.65,-0.24},
+			{0.64,-0.83,0.47,-0.35,-0.5,0.23},
+	};
+	cout << " Исходная система уравнений (расширенная матрица) : \n\n";
+	PrintMatrix(matrix);
+
+	for (int k = 0; k < 5; k++) {
+		Nulls(matrix, k);
+
+		MultDiag(matrix, k);
+
+		//Обнуление элементов k-го столбца
+		for (int i = k + 1; i < 5; i++) {
+			matrix[i][k] = 0;
+		}
+		// Деление всей строки на разрешающий элемент
+		for (int j = 5; j >= k; j--) {// int j = 5; j>= k; j—
+			matrix[k][j] = matrix[k][j] / matrix[k][k];
+		}
+
+		matrix[k][k] = matrix[k][k] / matrix[k][k];//Деление разрешающего элемента на самого себя
+		cout << endl << " Вывод: " << endl;
+		PrintMatrix(matrix);
+
+	}
+
+	//Обратный ход
+	int k = 0;
+
+	x[5] = matrix[4][5];
+	for (int i = 4; i >= 0; i--)
+	{
+		x[i] = matrix[i][5];
+		for (int j = i + 1; j < 5; j++) 
+			x[i] -= matrix[i][j] * x[j];
+	}
+
+
+	cout << endl << " Корни системы уравнений : " << endl;
+	for (int k = 0; k < 5; k++) {
+		cout << " X" << k + 1 << "=" << x[k] << "\n";
+	}
+	cout << endl;
+}
---
+++ lab1 Jordan-Gaysse/Lab1 Jordan/Lab1/Lab1.cpp
@@ -0,0 +1,96 @@
+﻿// Метод Жордана-Гаусса.
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <locale>
+#include <iomanip>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+void PrintMatrix(double(&matrix)[5][6]) {// Вывод матрицы
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 6; j++) {
+			if ((j + 1) % 6 == 0)
+				cout << setw(12) << fixed << setprecision(4) << matrix[i][j] << endl;
+			else
+				cout << setw(12) << fixed << setprecision(4) << matrix[i][j];
+		}
+	}
+
+}
+
+void Zamena(double(&matrix)[5][6], int x, int y) {// Смена местами строк 
+	double arr[6];
+	for (int j = 0; j < 6; j++) {
+		arr[j] = matrix[x][j];
+		matrix[x][j] = matrix[y][j];
+		matrix[y][j] = arr[j];
+	}
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	vector <double> vector(5);
+	double matrix[5][6]{
+			{-0.38,0.88,-0.4,-0.16,0.56,-0.79},
+			{0.75,0.88,0.46,0.32,0.16,0.07},
+			{-0.68,0.64,0.57,-0.88,-0.4,-0.52},
+			{0.48,-0.27,-0.45,-0.01,-0.65,-0.24},
+			{0.64,-0.83,0.47,-0.35,-0.5,0.23},
+	};
+
+	cout << " Исходная система уравнений (расширенная матрица) : \n\n";
+	PrintMatrix(matrix);
+	for (int k = 0; k < 5; k++) {// кусочек кода для проверки 0 на главной диагонали и перестановки !=0 значения
+		if (matrix[k][k] == 0)
+		{
+			int rows = k + 1;
+			while (matrix[k][k] == 0) {
+				if ((matrix[rows][k] != 0) || (rows > 5)) {
+					Zamena(matrix, rows, k);
+					cout << endl;
+					PrintMatrix(matrix);
+				}
+				rows++;
+			}
+		} 
+		// Правило прямоугольника
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = k + 1; j < 6; j++)
+			{
+				if (i != k && j > k)
+				{
+					matrix[i][j] = (matrix[i][j] * matrix[k][k] - matrix[i][k] * matrix[k][j]) / matrix[k][k];
+
+				}
+			}
+		}
+		// обнуление элементов i столбца
+		for (int i = 0; i <= 5 - 1; i++) { 
+			if (i != k)
+				matrix[i][k] = 0;
+		}
+		// деление элементов
+		for (int j = 5; j>=k; j--) { 
+			matrix[k][j] = matrix[k][j] / matrix[k][k];
+		}
+		matrix[k][k] = matrix[k][k] / matrix[k][k]; // х3 для чего и без него работает
+		cout << endl << " Вывод: " << endl;
+		PrintMatrix(matrix);
+
+	}
+		
+	cout << endl << " Корни системы уравнений : " << endl;
+	for (int i = 0; i < 5; i++) 
+	{
+		cout << " X" << i+1 << "=" << matrix[i][5] << "\n";
+	}
+	cout << endl;
+	system("pause ");
+	return 0;
+}
